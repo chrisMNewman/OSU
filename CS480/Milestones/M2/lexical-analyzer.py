4c8395edfc54f2404d7ab09b24e2f37380dd9ef6
Sang Shin <shinsa@onid.orst.edu>
2014-01-27 18:48:54 -0800

    Updated lexical-analyzer.py

--- CS480/Milestones/M2/lexical-analyzer.py
+++ CS480/Milestones/M2/lexical-analyzer.py
@@ -1,6 +1,49 @@
+# -------------------------------------
+# file      : lexical-analyzer.py
+# date      : 01-27-2014
+# member    : Sang Shin | Chris Newman 
+# class     : CS 480
+# Assign    : Milestone #2
+# -------------------------------------
 #!/usr/bin/python
 
+type_names = [
+        "NAME",
+        "NUMBER",
+        "STRING",
+        "NEWLINE",
+        "LPAR",
+        "RPAR",
+        "COLON",
+        "COMMA",
+        "SEMI",
+        "PLUS",
+        "MINUS",
+        "STAR",
+        "SLASH",
+        "VBAR",
+        "AMPER",
+        "LESS",
+        "GREATER",
+        "EQUAL",
+        "DOT",
+        "PERCENT",
+        "BACKQUOTE",
+        "LBRACE",
+        "RBRACE",
+        "EQEQUAL",
+        "NOTEQUAL",
+        "LESSEQUAL",
+        "GREATEREQUAL",
+        "TILDE",
+        "AT"
+        ]
+
 class Token(object):
+    '''
+    Token Structure.
+    Input: typ, val, pos
+    '''
     def __init__(self, typ, val, pos):
         self.typ = typ
         self.val = val
