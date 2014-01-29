0f6d171f9be90a43bef683860e1a8f25f13b448b
Sang Shin <shinsa@onid.orst.edu>
2014-01-29 11:58:44 -0800

    added example-c-file.py and removed draft2

---
+++ CS480/Milestones/M2/example-c-file.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int i, j = 1, k=2, flag;
+    for(i=0;i<=4;i++)
+    {
+        k=i+j;
+    }
+    flag=5;
+    if(flag==10)
+        printf("Hello Students");
+    getch();
+}
